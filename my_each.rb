79799e70353dce23741f34fd879da7f077688ee6
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-20 18:47:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+ require 'pry'
+def my_each(array)
+
+  if block_given?
+    counter = 0
+ 
+    while array < array.cycle(array) do array
+      yield(i)
+      binding.pry
+      counter += 1
+    end
+    array
+  end
 end
+
