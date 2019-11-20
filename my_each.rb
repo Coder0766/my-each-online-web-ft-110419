165301f91f983e2b68cb6de43c09d84e85500cea
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-20 19:49:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -7,8 +7,8 @@ def my_each(array)
     #we want our block to run over  each element in the array  
     
     while counter < array.length 
-    #we want to pass individual elements to the block 
-      yield(i)
+      #we want to pass individual elements to the block, and we want to pass the element that corresponds to which iteration we're in.  
+      yield()
       binding.pry
       counter += 1
     end
